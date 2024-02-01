a86fc40461b7e448572e3ecb23598f4ef3d09751
Amanda <collantesaart@gmail.com>
2024-02-01 14:43:53 -0500

    linking hero

--- src/pages/HomePage/Homepage.js
+++ src/pages/HomePage/Homepage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Main from "../../components/Main/Main";
+import Hero from "../../components/Hero/Hero";
 
 const HomePage = () => {
   return (
     <div>
+      <Hero />
       <Main />
     </div>
   );
