e2200e765d0dd8034ba25e008273ef763bfd75e9
Amanda <collantesaart@gmail.com>
2024-02-01 15:11:32 -0500

    finished hero header

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
 import "./Hero.scss";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <>
+      <div className="hero">
+        <Link to="/" className="HomePage">
+          <img className="logo-1" src={logo} alt="logo" />
+        </Link>
+        <div className="hero__title">
+          Make relevant memes to express your mood with Tejal & Amanda{" "}
+          <span role="img" aria-label="smiley emoji">
+            &#x1F60A;
+          </span>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Hero;
