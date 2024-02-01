628f580dfbee30e535a99661fb39b8b9d7e054aa
Amanda <collantesaart@gmail.com>
2024-02-01 14:18:42 -0500

    styled not found

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./styles/global.scss";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./pages/HomePage/Homepage";
 import NotFound from "./pages/NotFound/NotFound";
-import Main from "./components/Main/Main";
 
 function App() {
   return (
