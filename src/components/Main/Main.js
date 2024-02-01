d6ab3d7b2330df16161386235d50ce1086669e6d
TejalAGunjal <tejal.a.gunjal@gmail.com>
2024-02-01 11:38:58 -0500

    form and button added to main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,6 +8,24 @@ const GenerateMeme = () => {
   return (
     <section ClassName="meme">
       <h1>Generate your meme </h1>
+
+      <form className="meme__form" onSubmit={handleSubmit}>
+        <div className="meme__formwrapper">
+          <label className="meme__labelname" htmlFor="label">
+            Enter Top Text:
+          </label>
+          <input className="meme_input" type="text" placeholder="Add a text" />
+
+          <label className="meme__labelname" htmlFor="label">
+            Enter Bottom Text:
+          </label>
+          <input className="meme_input" type="text" placeholder="Add a text" />
+        </div>
+
+        <div className="meme__button">
+          <button className="meme__click">Generate Meme</button>
+        </div>
+      </form>
     </section>
   );
 };
