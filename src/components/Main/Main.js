51022491df7dbb3dfa25e987149a11bac1c1e4c2
TejalAGunjal <tejal.a.gunjal@gmail.com>
2024-02-01 11:09:59 -0500

    Heading added to main.js

---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import "./Main.scss";
+
+const GenerateMeme = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <section ClassName="meme">
+      <h1>Generate your meme </h1>
+    </section>
+  );
+};
+
+export default GenerateMeme;
