17bd6eff372dad755e68569f3cc857e50f941689
Amanda <collantesaart@gmail.com>
2024-02-01 13:06:18 -0500

    finished footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Added import for Link component
 import "./Footer.scss";
+import logo from "../../assets/logo.png";
+import fb from "../../assets/icons/fb.png";
+import ig from "../../assets/icons/ig.png";
+import t from "../../assets/icons/t.png";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <h4 className="footer__title">Meme Generator</h4>
+      <div className="footer__wrapper">
+        <Link to="/" className="HomePage">
+          <img className="logo" src={logo} alt="logo" />
+        </Link>
+        <div className="footer__socials">
+          <h4 className="footer__socials--title"> Connect with us!</h4>
+          <div className="footer__icons">
+            <a href="*">
+              <img className="fb" src={fb} alt="facebook" />
+            </a>
+            <a href="*">
+              <img className="ig" src={ig} alt="instagram" />
+            </a>
+            <a href="*">
+              <img className="t" src={t} alt="twitter" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
